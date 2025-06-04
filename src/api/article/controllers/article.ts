e877eb136e20684ceb292c372ff4e9680c7c49a2
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-06-04 17:26:07 +0300

    счётчик просмотров

--- src/api/article/controllers/article.ts
+++ src/api/article/controllers/article.ts
@@ -2,6 +2,50 @@
  *  article controller
  */
 
+/*
+
 import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::article.article');
+*/
+
+"use strict";
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::article.article", ({ strapi }) => ({
+  // Основной метод для увеличения просмотров
+  async incrementViews(ctx) {
+    try {
+      const { documentId } = ctx.params;
+
+      // Находим статью по documentId
+      const article = await strapi.db.query("api::article.article").findOne({
+        where: { documentId },
+        select: ["id", "views"],
+      });
+
+      if (!article) {
+        return ctx.notFound("Article not found");
+      }
+
+      // Обновляем счётчик
+      const updatedArticle = await strapi.db
+        .query("api::article.article")
+        .update({
+          where: { id: article.id },
+          data: {
+            views: (article.views || 0) + 1,
+          },
+          select: ["views"], // Возвращаем только обновлённое количество просмотров
+        });
+
+      return {
+        success: true,
+        views: updatedArticle.views,
+      };
+    } catch (err) {
+      ctx.throw(500, "Internal server error", { details: err });
+    }
+  },
+}));
--- src/api/article/routes/article.ts
+++ src/api/article/routes/article.ts
@@ -1,7 +1,25 @@
 /**
  * article router.
  */
-
+/*
 import { factories } from '@strapi/strapi';
 
 export default factories.createCoreRouter('api::article.article');
+*/
+
+"use strict";
+
+module.exports = {
+  routes: [
+    {
+      method: "POST",
+      path: "/articles/:documentId/increment-views",
+      handler: "article.incrementViews",
+      config: {
+        auth: false,
+        policies: [],
+        middlewares: [],
+      },
+    },
+  ],
+};
